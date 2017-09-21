d02f83c1cae6f425b0d7cee9101ace834ae290be
ccmoralesj <ccmj202@gmail.com>
2017-09-21 15:47:19 -0500

    feat(TEam): Team Interface ready

--- interfaces/team.js
+++ interfaces/team.js
@@ -1,2 +1,48 @@
+const logger = require('winston');
+const TeamModel = require('../schemas').Team;
+const errors = require('../utils/errors');
 
-module.exports = {};
+const readById = async (id) => {
+  logger.info('Looking for Team with id', id);
+  const teamFound = await TeamModel.findById(id).lean();
+  logger.verbose('Team Found', teamFound);
+  return teamFound;
+};
+/**
+ * @param query
+ * @returns Team Array
+ */
+const read = async (query = {}) => {
+  logger.info('Looking for Team');
+  logger.info('query:', query);
+  const teamsFound = await TeamModel.find(query).lean();
+  logger.verbose('Teams Found', teamsFound);
+  return teamsFound;
+};
+
+const create = async ({ name, birthDate }) => {
+  logger.info(`Attempting to create a Team with name: ${name} and birthDate: ${birthDate}`);
+  if (!name) throw new Error(errors.NAME_REQUIRED);
+  const teamCreated = await TeamModel.create({ name, birthDate });
+  logger.info('Team created successfully');
+  return teamCreated.toObject();
+};
+
+const update = async ({ id, name, birthDate } = {}) => {
+  logger.info(`Looking up to update team with id: ${id}`);
+  const attrToUpdate = {};
+  if (name) attrToUpdate.name = name;
+  if (birthDate) attrToUpdate.birthDate = birthDate;
+  const updated = await TeamModel.update({ _id: id }, attrToUpdate, { runValidators: true, overwrite: false });
+  logger.verbose(`Updated: ${updated}`);
+  return updated;
+};
+
+const remove = async (id) => {
+  logger.info('Removing Team with id', id);
+  const teamDeleted = await TeamModel.remove({ _id: id });
+  logger.verbose(`Team removed ${teamDeleted}`);
+  return teamDeleted;
+};
+
+module.exports = { readById, read, create, update, remove };
---
+++ test/test-team.js
@@ -0,0 +1,98 @@
+const chai = require('chai');
+require('chai-as-promised');
+const roles = require('../utils/roles');
+const errors = require('../utils/errors');
+const DailyScrum = require('../index');
+const expect = chai.expect;
+
+describe('Team Interface', () => {
+  let firstTeamId;
+  let secondTeamId;
+  const firstTeam = {
+    name: 'Team Test',
+  };
+  const secondTeam = {
+    name: 'Test Team',
+  };
+  describe('Team Creation', () => {
+    it('Should return a validation error NAME_REQUIRED', async () =>{
+      try {
+        await DailyScrum.Interfaces.Team.create({});
+      } catch (e) {
+        expect(e.message).to.be.eql(errors.NAME_REQUIRED);
+      }
+    });
+    it('Should create a Team', async () => {
+      const newTeam = await DailyScrum.Interfaces.Team.create(firstTeam);
+      expect(newTeam).to.has.property('id');
+      firstTeamId = newTeam.id;
+      expect(newTeam).to.has.property('name').eql(firstTeam.name);
+    });
+    it('Should create another Team', async() => {
+      const newTeam = await DailyScrum.Interfaces.Team.create(secondTeam);
+      expect(newTeam).to.has.property('id');
+      secondTeamId = newTeam.id;
+      expect(newTeam).to.has.property('name').eql(secondTeam.name);
+    });
+  });
+  describe('Team Read', () => {
+    it('Should read two teams', async () => {
+      const teams = await DailyScrum.Interfaces.Team.read();
+      expect(teams).to.be.an('Array').of.lengthOf(2);
+      expect(teams[0]).to.has.property('name').eql(firstTeam.name);
+      expect(teams[1]).to.has.property('name').eql(secondTeam.name);
+    });
+    it('Should find a specific team', async() => {
+      const teamFound = await DailyScrum.Interfaces.Team.readById(firstTeamId);
+      expect(teamFound).to.has.property('id').eql(firstTeamId);
+      expect(teamFound).to.has.property('name').eql(firstTeam.name);
+    });
+    it('Shouldn\'t find a team', async () => {
+      const fakeId = 'testIdToFail';
+      const teamNotFound = await DailyScrum.Interfaces.Team.readById(fakeId);
+      expect(teamNotFound).to.be.null;
+    });
+  });
+  describe('Team Update', () => {
+    it('Should update a Team', async () => {
+      const testName = 'Another Test Name';
+      const updated = await DailyScrum.Interfaces.Team.update({ id: secondTeamId, name: testName });
+      expect(updated).to.has.property('ok').eql(1);
+      expect(updated).to.has.property('nModified').eql(1);
+      expect(updated).to.has.property('n').eql(1);
+
+      const teamFound = await DailyScrum.Interfaces.Team.readById(secondTeamId);
+      expect(teamFound).to.has.property('id').eql(secondTeamId);
+      expect(teamFound).to.has.property('name').eql(testName);
+    });
+    it('Shouldn\'t update a Team', async () => {
+      const fakeId = 'testIdToFail';
+      const testName = 'Another Test Name';
+      const updated = await DailyScrum.Interfaces.Team.update({ id: fakeId, name: testName });
+      expect(updated).to.has.property('ok').eql(1);
+      expect(updated).to.has.property('nModified').eql(0);
+      expect(updated).to.has.property('n').eql(0);
+    });
+  });
+  describe('Team Delete', () => {
+    it('Should delete a Team', async () => {
+      const teamDeleted = await DailyScrum.Interfaces.Team.remove(firstTeamId);
+      expect(teamDeleted).to.has.property('result');
+      expect(teamDeleted.result).to.has.property('ok').eql(1);
+      expect(teamDeleted.result).to.has.property('n').eql(1);
+    });
+    it('Should delete a Team', async () => {
+      const teamDeleted = await DailyScrum.Interfaces.Team.remove(secondTeamId);
+      expect(teamDeleted).to.has.property('result');
+      expect(teamDeleted.result).to.has.property('ok').eql(1);
+      expect(teamDeleted.result).to.has.property('n').eql(1);
+    });
+    it('Shouldn\'t delete a Team', async () => {
+      const fakeId = 'testIdToFail';
+      const teamDeleted = await DailyScrum.Interfaces.Team.remove(fakeId);
+      expect(teamDeleted).to.has.property('result');
+      expect(teamDeleted.result).to.has.property('ok').eql(1);
+      expect(teamDeleted.result).to.has.property('n').eql(0);
+    });
+  });
+});
